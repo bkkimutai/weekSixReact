4e72772f8c383ba5735ba8eeede177bd94949bb1
bkkimutai <kbmsama@gmail.com>
2023-07-21 09:58:02 +0300

    a

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
    // Replace '*' with the specific origin of your frontend (http://localhost:3000)
-   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+   res.header('Access-Control-Allow-Origin', '*');
    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
    next();
  });
