2df333d90d523b9b31000d6d47bb501b28fa7ebd
bkkimutai <kbmsama@gmail.com>
2023-07-21 10:45:34 +0300

    cors before routing update

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const productRoutes = require('./routes/productRoutes');
 const getDatabase = require('./config/database');
 const cors = require('cors');
-
 const app = express()
+app.use(cors());
 // add middleware & static files
 app.use(express.json()) //Body parser
 app.use("/api/product", productRoutes);
 //app.use("/api/user",userRoutes);
-app.use(cors());
+
 let db = getDatabase();
 const PORT = process.env.PORT || 5000        // Define the PORT
 
