328e324598de2157fd78a70280cf5656fc3f2221
bkkimutai <kbmsama@gmail.com>
2023-07-25 08:22:54 +0300

    routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -36,33 +36,44 @@ router.post('/addproduct', async (req, res) => {
   
 // Update a product by its ID
 router.put('/updateproduct/:id', async (req, res) => {
-    try {
-      const productId = req.params.id;
-      const updatedProductData = req.body;
-      // Find the product by its ID
-      const product = await Product.findById(productId);
-      if (!product) {
-        return res.status(404).json({ message: 'Product not found' });
-      }
+      // Update a product in the database
+      Product.updateOne({_id:req.params.id},{
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        quantity: req.body.quantity,
+    }, {upsert: true}, (err)=>{
+        if(err) console.log(err);
+        res.json({success:true})
+    })
+})
+  //   try {
+  //     const productId = req.params.id;
+  //     const updatedProductData = req.body;
+  //     // Find the product by its ID
+  //     const product = await Product.findById(productId);
+  //     if (!product) {
+  //       return res.status(404).json({ message: 'Product not found' });
+  //     }
   
-      // Update the product fields with the new data
-      product.name = updatedProductData.name;
-      product.description = updatedProductData.description;
-      product.price = updatedProductData.price;
-      product.quantity = updatedProductData.quantity;
-      product.id = updatedProductData.id;
-      product.photo = updatedProductData.photo;
-      product.deleted = updatedProductData.deleted
+  //     // Update the product fields with the new data
+  //     product.name = updatedProductData.name;
+  //     product.description = updatedProductData.description;
+  //     product.price = updatedProductData.price;
+  //     product.quantity = updatedProductData.quantity;
+  //     product.id = updatedProductData.id;
+  //     product.photo = updatedProductData.photo;
+  //     product.deleted = updatedProductData.deleted
   
-      // Save the updated product to the database
-      await product.save();
+  //     // Save the updated product to the database
+  //     await product.save();
   
-      res.json(product);
-    } catch (error) {
-      console.error('Error updating product:', error);
-      res.status(500).json({ message: 'Server error' });
-    }
-  });
+  //     res.json(product);
+  //   } catch (error) {
+  //     console.error('Error updating product:', error);
+  //     res.status(500).json({ message: 'Server error' });
+  //   }
+  // });
   
 // @route DELETE api/product/products/:id
 // Delete a product by its ID (Soft Delete)
