3c3728e9f41ab256eee78c4ce4008b8b8879e12e
bkkimutai <kbmsama@gmail.com>
2023-08-04 16:11:25 +0300

    login session longer

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const session = require('express-session');
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
 const getDatabase = require('./config/database');
+const crypto = require('crypto');
 const cors = require('cors');
 const app = express()
 app.use(cors());
+const secretKey = crypto.randomBytes(64).toString('hex');
+app.use(
+   session({
+     secret: secretKey, // Replace with your secret key
+     resave: false,
+     saveUninitialized: true,
+     cookie: {
+       secure: false, // Set to true if using HTTPS
+       maxAge: 86400000, // Set the session timeout in milliseconds (e.g., 24 hours)
+     },
+   })
+ );
 // add middleware & static files
 app.use(express.json()) //Body parser
 app.use("/api/products", productRoutes);
